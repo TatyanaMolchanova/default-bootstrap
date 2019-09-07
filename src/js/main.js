79cd8b9ce60b283621ed54c5d6f51d61a7d89783
Molchanova Tatyana <mtuspeh@gmail.com>
2019-09-07 18:20:12 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+//= lib/jquery.js
+//= lib/slick.js
+//= lib/popper.js
+
+
+//= bootstrap/util.js
+//= bootstrap/collapse.js
+//= bootstrap/dropdown.js
+
+///= bootstrap/alert.js
+///= bootstrap/button.js
+///= bootstrap/carousel.js
+///= bootstrap/modal.js
+///= bootstrap/tooltip.js
+///= bootstrap/popover.js
+///= bootstrap/scrollspy.js
+///= bootstrap/tab.js
+///= bootstrap/toast.js
+
+
+
+//= scripts.js
